1bda92b02b60fc7759b4209e3cebd4ef9474c4be
juanrodriguez <juan.rodriguez@boomi.com>
2023-04-20 16:48:17 -0400

    third commit

--- lambda/iot_package.sh
+++
@@ -1,5 +0,0 @@
-
-cd /Users/juanrodriguez/Documents/GPT_Python/IoT_SIM/lib/python3.9/site-packages
-zip -r ../../../../my-deployment-package.zip .
-cd ../../../lambda
-zip -g ../../my-deployment-package.zip lambda_function.py
